240baf6694aca548f81d34cadd8d57c38cbe0be3
Emerson Martins <emerson.garcao@gmail.com>
2023-10-04 15:59:29 -0300

    Atualização de nomes dos assets

--- script.js
+++ script.js
@@ -6,10 +6,10 @@ function toggleMode(){
   //alteração de fotos do profile
   const img = document.querySelector("#profile img")
   if(html.classList.contains("light")){
-    img.setAttribute("src", "./assets/avatar-light.png")
+    img.setAttribute("src", "./assets/Avatar-Light.png")
     img.setAttribute("Alt","Foto de Emerson Martins com camisa clara óculos escuros e fundo amarelo")
   } else {
-    img.setAttribute("src", "./assets/avatar-dark.png")
+    img.setAttribute("src", "./assets/Avatar-dark.png")
     img.setAttribute("Alt","Foto de Emerson Martins com terno e gravata azul, barba e cabelo penteado para trás e plano de fundo azul escuro.")
   }
 
