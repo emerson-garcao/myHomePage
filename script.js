145e6aa906cc540d7ca1405b4cfba5c6388f4ea0
Emerson Martins <emerson.garcao@gmail.com>
2023-10-04 15:15:47 -0300

    alteração da descrição da foto em caso de não carregamento.

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 function toggleMode(){
+  //inserir light ao clicar
   const html = document.documentElement
   html.classList.toggle("light")
+
+  //alteração de fotos do profile
   const img = document.querySelector("#profile img")
   if(html.classList.contains("light")){
     img.setAttribute("src", "./assets/avatar-light.png")
+    img.setAttribute("Alt","Foto de Emerson Martins com camisa clara óculos escuros e fundo amarelo")
   } else {
     img.setAttribute("src", "./assets/avatar-dark.png")
+    img.setAttribute("Alt","Foto de Emerson Martins com terno e gravata azul, barba e cabelo penteado para trás e plano de fundo azul escuro.")
   }
+
+  
 }
